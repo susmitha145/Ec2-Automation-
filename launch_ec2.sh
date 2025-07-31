eb85b0b2a531fe62a4d457184662fb22b0e88300
susmitha145 <mksush17@gmail.com>
2025-07-31 22:06:52 +0530

    Update script with SG ID and Subnet ID

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Launch EC2 Instance using AWS CLI
-
 aws ec2 run-instances \
   --image-id ami-0c02fb55956c7d316 \
   --instance-type t2.micro \
   --key-name susmitha-key \
-  --security-groups jenkins-sg \
-  --region us-east-1
+  --security-group-ids sg-0973a8e6c99e52bf4 \
+  --subnet-id subnet-09d648e38a0cd155e \
+  --region ap-south-1
+
