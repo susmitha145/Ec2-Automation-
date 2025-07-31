a957c6abe431e51e41c2531e89f184618c331c18
susmitha145 <mksush17@gmail.com>
2025-07-31 22:14:51 +0530

    Update launch_ec2.sh

--- launch_ec2.sh
+++ launch_ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws ec2 run-instances \
-  --image-id ami-0c02fb55956c7d316 \
+  --image-id ami-03bb6d83c60fc5f7c \
   --instance-type t2.micro \
   --key-name susmitha-key \
   --security-group-ids sg-0973a8e6c99e52bf4 \
